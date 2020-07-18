b8d453037841c8c7236f2f1129a9cd8d4c3d7d7b
phillima <phyllipe_slf@yahoo.com.br>
2020-07-17 22:28:39 -0300

    Aula 1 - Maven

---
+++ aula1-maven/src/main/java/br/inatel/cdg/inimigo/Inimigo.java
@@ -0,0 +1,12 @@
+package br.inatel.cdg.inimigo;
+
+public class Inimigo {
+	
+	private String nome;
+	private double vida;
+	
+	public Inimigo(String nome, double vida) {
+		this.nome = nome;
+		this.vida = vida;
+	}
+}

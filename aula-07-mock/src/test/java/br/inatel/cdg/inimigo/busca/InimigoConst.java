64864d18b873cbbb2610b58f4bdff7f29bc9d6c1
phillima <phyllipe_slf@yahoo.com.br>
2020-09-02 10:00:02 -0300

    Inimigo Const Dummy Class

---
+++ aula-07-mock/src/test/java/br/inatel/cdg/inimigo/busca/InimigoConst.java
@@ -0,0 +1,14 @@
+package br.inatel.cdg.inimigo.busca;
+
+public class InimigoConst {
+	
+	public static String RINGED_KNIGHT = 
+	          "{ \"nome\": \"Ringed Knight\", \n " + 
+    		  "\"vida\": 200, \n " + 
+    		  "\"arma\": \"Ringed Knight Sword\" }";  
+
+	public static String HOLLOW =
+			"{ \"nome\": \"Hollow\", \n " + 
+		    "\"vida\": 20, \n " + 
+		    "\"arma\": \"Broken Sword\" }"; 
+}

13a712c616eb76dd28457b23c4f3d1a7b9bafb2b
phillima <phyllipe_slf@yahoo.com.br>
2020-09-16 10:49:32 -0300

    Aula-08-TDD AchivmentMessages

--- aula-08-tdd/src/main/java/br/inatel/cdg/aula_08/notifiers/SemRecebedoresException.java
+++ aula-08-tdd/src/main/java/br/inatel/cdg/aula_08/notifiers/SemRecebedoresException.java
@@ -2,25 +2,8 @@ package br.inatel.cdg.aula_08.notifiers;
 
 public class SemRecebedoresException extends RuntimeException {
 
-	public SemRecebedoresException() {
-		super();
-	}
-
-	public SemRecebedoresException(String message, Throwable cause, boolean enableSuppression,
-			boolean writableStackTrace) {
-		super(message, cause, enableSuppression, writableStackTrace);
-	}
-
-	public SemRecebedoresException(String message, Throwable cause) {
-		super(message, cause);
-	}
-
 	public SemRecebedoresException(String message) {
 		super(message);
-	}
-
-	public SemRecebedoresException(Throwable cause) {
-		super(cause);
 	}
 	
 }

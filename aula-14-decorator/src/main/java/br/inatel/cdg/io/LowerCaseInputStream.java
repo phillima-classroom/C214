d5270b825d9d420f87f2036d5e974f1aea94fbfa
phillima <phyllipe_slf@yahoo.com.br>
2020-11-03 12:08:30 -0300

    Aula-14-Decorator Java IO

---
+++ aula-14-decorator/src/main/java/br/inatel/cdg/io/LowerCaseInputStream.java
@@ -0,0 +1,22 @@
+package br.inatel.cdg.io;
+
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LowerCaseInputStream extends FilterInputStream {
+
+	public LowerCaseInputStream(InputStream in) {
+		super(in);
+	}
+	
+	@Override
+	public int read() throws IOException {
+		int c = in.read();
+		if(c == -1)
+			return c;
+		else
+			return Character.toLowerCase(c);
+	}
+
+}
---
+++ aula-14-decorator/src/test/java/br/inatel/cdg/io/teste/TesteLowerCaseInputStream.java
@@ -0,0 +1,71 @@
+package br.inatel.cdg.io.teste;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import br.inatel.cdg.io.LowerCaseInputStream;
+
+public class TesteLowerCaseInputStream {
+	
+	private String msgExpected = "quero jogar cthulu death may die";
+	private String msgOriginal = "Quero jogar Cthulu Death May Die";
+	private InputStream in = null;
+	private PrintStream pStream;
+	private String nomeArquivo = "teste.txt";
+	
+	@Before
+	public void init() {
+		
+		try{
+			pStream = new PrintStream(nomeArquivo);
+			pStream.print(msgOriginal);
+			pStream.close();
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testArquivoCreated() {
+		long tamanho = 0;
+		try {
+			tamanho = Files.size(Paths.get(nomeArquivo));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		assertEquals(32, tamanho);
+	}
+	
+	@Test
+	public void testFraseMinuscula() {
+		StringBuilder msgLida = new StringBuilder();
+		int c;
+		try {
+			in = new LowerCaseInputStream(new FileInputStream(nomeArquivo));
+			while((c = in.read()) >= 0) {
+				msgLida.append((char)c);
+			}
+			in.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		assertEquals(msgExpected, msgLida.toString());
+			
+	}
+
+}

286c562f4c7a8ddcfb81bd8725ea794766db6488
phillima <phyllipe_slf@yahoo.com.br>
2020-11-30 21:31:50 -0300

    Aula-17-Reflection GeradorPropriedade

--- aula-17-reflection/src/main/java/br/inatel/cdg/geradorpropriedade/GeradorMapa.java
+++ aula-17-reflection/src/main/java/br/inatel/cdg/geradorpropriedade/GeradorMapa.java
@@ -9,13 +9,22 @@ public class GeradorMapa {
 	public static Map<String, Object> gerarMapa(Object o){
 		Map<String,Object> mapa = new HashMap<String, Object>();
 		
+		//Busca a instancia que representa a classe da instancia "o"
 		Class<?> clazz = o.getClass();
 		
+		//Percorre todos os metodos que a classe possui
 		for (Method method : clazz.getMethods()) {
-			if(isGetter(method)) {
+			if(isGetter(method)) {//verifica se eh um getter
+				
+				//Busca o nome do membro da classe, baseado 
+				//no nome do metodo getter
 				String memberName = getMemberNome(method.getName());
 				try {
+					//Invoca o metodo getter para salvar o valor
+					//do membro que esta na instancia "o"
 					Object valor = method.invoke(o);
+					
+					//Armazena no mapa
 					mapa.put(memberName, valor);
 				} catch (Exception e) {
 					e.getStackTrace();
@@ -26,13 +35,16 @@ public class GeradorMapa {
 		return mapa;
 	}
 	
+	//Metodo para verificar se um metodo eh um getter
 	private static boolean isGetter(Method method) {
+		//verficia se comeca com a palavra "get"
 		return method.getName().startsWith("get") &&
-		method.getReturnType() != void.class &&
-		method.getParameterAnnotations().length == 0 &&
-		!method.getName().equals("getClass");
+		method.getReturnType() != void.class && //nao pode ser void
+		method.getParameterAnnotations().length == 0 && //nao pode ter parametros
+		!method.getName().equals("getClass"); //desconsidera o metodo "getClass"
 	}
 	
+	//Metodo que recupera o nome do membro pelo metodo getter
 	private static String getMemberNome(String getterName) {
 		StringBuilder builder = new StringBuilder();
 		
